e3ad9728467fbe4eb0e5e333f835f7807c1452af
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-11-14 19:40:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add simple interaction (optional)
+// document.querySelector(".cta-btn").addEventListener("click", () => {
+//   alert("Trial started!");
+// });
+function scrollToSection() {
+    document.getElementById("analyzeForm").scrollIntoView({
+        behavior: "smooth"
+    });
+}
